bd6967e91caa75eb7aa600dce963d41962eba2f0
tauhead <github@tauhead.com>
2019-01-17 21:46:23 +0000

    /item/list shows different columns for armor/weapon/medical item-types

--- lib/TauHead/Controller/Item/List.pm
+++ lib/TauHead/Controller/Item/List.pm
@@ -10,22 +10,66 @@ BEGIN { extends 'TauHead::BaseController' }
 sub list : Path('') : Args(0) : FormConfig {
     my ( $self, $c ) = @_;
 
-    my $form = $c->stash->{form};
-    my %dataTablesParams = $self->process_data_tables(
-        $c,
-        {
-            colNames => [
-                'me.name',
-                'me.tier',
-                'me.value',
-                'me.mass',
-                'vendor_items.price',
-                'vendor_items.price_unit',
-                # don't add column 6 - it's not a DBIx::Class field
+    my $default_columns = [
+        [ 'me.name'  => 'Name' ],
+        [ 'me.tier'  => 'Tier' ],
+        [ 'me.value' => 'Value' ],
+        [ 'me.mass'  => 'Mass' ],
+    ];
+
+    my %item_type_search = (
+        armor => {
+            join => 'item_component_armor',
+            columns => [
+                [ 'me.name'  => 'Name' ],
+                [ 'me.tier'  => 'Tier' ],
+                [ 'item_component_armor.energy'   => 'Energy' ],
+                [ 'item_component_armor.impact'   => 'Impact' ],
+                [ 'item_component_armor.piercing' => 'Piercing' ],
             ],
-            sSortDir_0 => 'asc',
+        },
+        medical => {
+            join => 'item_component_medical',
+            columns => [
+                [ 'me.name'  => 'Name' ],
+                [ 'me.tier'  => 'Tier' ],
+                [ 'item_component_medical.base_toxicity'  => 'Toxicity' ],
+                [ 'item_component_medical.strength_boost' => 'Str' ],
+                [ 'item_component_medical.agility_boost'  => 'Agi' ],
+                [ 'item_component_medical.stamina_boost'  => 'Sta' ],
+            ],
+        },
+        weapon => {
+            join => 'item_component_weapon',
+            columns => [
+                [ 'me.name'  => 'Name' ],
+                [ 'me.tier'  => 'Tier' ],
+                [ 'item_component_weapon.energy_damage'   => 'Energy' ],
+                [ 'item_component_weapon.impact_damage'   => 'Impact' ],
+                [ 'item_component_weapon.piercing_damage' => 'Piercing' ],
+                [ 'item_component_weapon.accuracy'    => 'Accuracy' ],
+                [ 'item_component_weapon.weapon_type' => 'Type' ],
+            ],
         },
     );
+
+    my $item_type = scalar $c->request->param('item_type');
+    if ( defined( $item_type ) && $item_type =~ /^[a-z-]+\z/ ) {
+        $item_type = $c->model('DB')->resultset('ItemType')->find({ slug => $item_type })
+    }
+
+    my @add_join;
+    my @use_columns;
+    if ( $item_type && exists $item_type_search{ $item_type->slug } ) {
+        my $spec = $item_type_search{ $item_type->slug };
+        push @add_join, $spec->{join};
+        @use_columns = @{ $spec->{columns} };
+    }
+    else {
+        @use_columns = @$default_columns;
+    }
+
+    my @use_as = map { my $x = $_->[0]; $x =~ s/\./_/g; $x } @use_columns;
 
     my @known_params = qw(
         stale
@@ -33,77 +77,66 @@ sub list : Path('') : Args(0) : FormConfig {
     );
     $c->stash->{known_params} = \@known_params;
 
-    my %cond = (
-        'vendor_items.id' => [
-            '-or' => {
-                '=' => $c->model('DB')->resultset('VendorItem')->search(
-                    {
-                        'item_slug' => { -ident => 'me.slug' },
-                    },
-                    {
-                        alias    => 'vi',
-                        group_by => ['vi.id'],
-                        order_by => { -asc => 'vi.price' },
-                        rows     => 1,
-                    },
-                )->get_column('id')->as_query,
-            }, {
-                '=' => undef,
-            }
-        ]
+    my %search_cond = ();
+    my %search_attrs = (
+        '+select' => [ map { $_->[0] } @use_columns ],
+        '+as'     => \@use_as,
     );
 
     if ( $c->request->param('stale') ) {
-        $cond{'description'} = '';
+        $search_cond{description} = '';
     }
 
-    my $item_type = scalar $c->request->param('item_type');
-    my $type;
-    if ( defined( $item_type )
-        && ( $item_type =~ /^[a-z-]+\z/ )
-        && ( $type = $c->model('DB')->resultset('ItemType')->find({ slug => $item_type }) )
-        )
-    {
-        $cond{'item_type_slug'} = $item_type;
-        $c->stash->{legend} = $type->name;
+    if ( $item_type ) {
+        $search_cond{item_type_slug} = $item_type->slug;
+        $c->stash->{legend} = $item_type->name;
     }
+
+    if ( @add_join ) {
+        $search_attrs{join} = \@add_join;
+    }
+
+    $c->stash->{column_labels} = [
+        ( map { $_->[1] } @use_columns ),
+        'In-game Link',
+    ];
 
     return unless $c->request->accepts('application/json');
 
-    my $rs = $c->model('DB')->resultset('Item')->search(
-        \%cond,
+    my %dataTablesParams = $self->process_data_tables(
+        $c,
         {
-            join => [ 'item_type', 'vendor_items' ],
-            '+select' => [ 'item_type.name', 'vendor_items.price', 'vendor_items.price_unit' ],
-            '+as'     => [ 'item_type_name', 'vendor_item_price', 'vendor_item_price_unit' ],
-            %{ $dataTablesParams{attrs} },
+            colNames   => [ map { $_->[0] } @use_columns ],
+            sSortDir_0 => 'asc',
         },
+    );
+
+    %search_attrs = (
+        %search_attrs,
+        %{ $dataTablesParams{attrs} },
+    );
+
+    my $rs = $c->model('DB')->resultset('Item')->search(
+        \%search_cond,
+        \%search_attrs,
     );
     my @rows;
 
     while ( my $item = $rs->next ) {
         my @cols;
-        # column 1 - name / link
-        push @cols, sprintf q{<a href="%s" class="list-title">%s</a>},
-            $c->uri_for( '/item', $item->slug )->as_string,
-            $item->name;
+        for my $column (@use_as) {
+            if ( 'me_name' eq $column ) {
+                # column 1 - name / link
+                push @cols, sprintf q{<a href="%s" class="list-title">%s</a>},
+                    $c->uri_for( '/item', $item->slug )->as_string,
+                    $item->name;
+            }
+            else {
+                push @cols, $item->get_column($column);
+            }
+        }
 
-        # column 2 - tier
-        push @cols, $item->tier;
-
-        # column 3 - value
-        push @cols, $item->value;
-
-        # column 4 - mass
-        push @cols, $item->mass;
-
-        # column 5 - price
-        push @cols, $item->get_column('vendor_item_price');
-
-        # column 6 - price_unit
-        push @cols, ucfirst $item->get_column('vendor_item_price_unit');
-
-        # column 7 - in-game link
+        # last column - in-game link
         push @cols, sprintf '<a href="%s" target="_blank">%s %s</a>',
             $item->in_game_link( $c ),
             'View in-game',
