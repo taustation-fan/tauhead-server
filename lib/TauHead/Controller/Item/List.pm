11b11dd02effbab79b038e9c959f3bfa9d512973
tauhead <github@tauhead.com>
2019-01-18 19:36:46 +0000

    /item/list filter by tier & weapon-type
    TODO: cleanup code. separate html/json generating code. use FormFu

--- lib/TauHead/Controller/Item/List.pm
+++ lib/TauHead/Controller/Item/List.pm
@@ -53,9 +53,31 @@ sub list : Path('') : Args(0) : FormConfig {
         },
     );
 
+    my %search_cond = ();
+    my $item_rs = $c->model('DB')->resultset('Item');
+
     my $item_type = scalar $c->request->param('item_type');
     if ( defined( $item_type ) && $item_type =~ /^[a-z-]+\z/ ) {
-        $item_type = $c->model('DB')->resultset('ItemType')->find({ slug => $item_type })
+        $c->stash->{item_type} = $item_type;
+
+        if ( 'weapon' eq $item_type ) {
+            $c->stash->{weapon_types} = $c->model('DB')->resultset('ItemComponentWeapon')->search(
+                    undef,
+                    {
+                        select   => ['weapon_type'],
+                        as       => ['weapon_type'],
+                        distinct => 1,
+                        order_by => ['weapon_type'],
+                    }
+                )->get_column('weapon_type');
+
+                my $search_weapon = scalar $c->request->param('sSearch_6');
+                if ( defined( $search_weapon ) && $search_weapon =~ /^[\w\s]+\z/ ) {
+                    $search_cond{'item_component_weapon.weapon_type'} = $search_weapon;
+                }
+        }
+
+        $item_type = $c->model('DB')->resultset('ItemType')->find({ slug => $item_type });
     }
 
     my @add_join;
@@ -77,11 +99,15 @@ sub list : Path('') : Args(0) : FormConfig {
     );
     $c->stash->{known_params} = \@known_params;
 
-    my %search_cond = ();
     my %search_attrs = (
         '+select' => [ map { $_->[0] } @use_columns ],
         '+as'     => \@use_as,
     );
+
+    my $tier = scalar $c->request->param('sSearch_1');
+    if ( defined( $tier )  && $tier =~ /^[0-9]+\z/ ) {
+        $search_cond{'me.tier'} = $tier;
+    }
 
     if ( $c->request->param('stale') ) {
         $search_cond{description} = '';
@@ -101,6 +127,8 @@ sub list : Path('') : Args(0) : FormConfig {
         'In-game Link',
     ];
 
+    $c->stash->{max_tier} = $item_rs->get_column('tier')->max;
+
     return unless $c->request->accepts('application/json');
 
     my %dataTablesParams = $self->process_data_tables(
@@ -116,7 +144,7 @@ sub list : Path('') : Args(0) : FormConfig {
         %{ $dataTablesParams{attrs} },
     );
 
-    my $rs = $c->model('DB')->resultset('Item')->search(
+    my $rs = $item_rs->search(
         \%search_cond,
         \%search_attrs,
     );
